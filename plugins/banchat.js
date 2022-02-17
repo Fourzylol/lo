53ec85387cbc9827e4cc761742a0c62a0db1939d
Fourzy <94512377+Fourzylol@users.noreply.github.com>
2022-02-17 22:37:58 +0700

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,7 +1,7 @@
-let handler = async (m, { conn, isOwner, text }) => {
+let handler = async (m, { conn, isOwner, text, isAdmin }) => {
   let who
   if (m.isGroup) {
-    if (!isOwner)) return dfail('owner', m, conn)
+    if (!(isAdmin || isOwner)) return dfail('owner', m, conn)
     if (isOwner) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
     else who = m.chat
   } else {
